a34c31985847eb05245ea1548287e6bd883a11c9
Fede <federicoameratte@gmail.com>
2022-06-07 16:26:24 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="App-logo" alt="logo">Hola Mundo</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
