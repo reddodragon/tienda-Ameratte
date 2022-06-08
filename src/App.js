29efada1cb4fcb60d0a00b0d37b39598ff9afac5
Fede <federicoameratte@gmail.com>
2022-06-08 15:00:02 -0300

    agregando carpeta components y navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-
+import ResponsiveAppBar from './components/NavBar'
 function App() {
   return (
+
     <div className="App">
+      <ResponsiveAppBar></ResponsiveAppBar>
       <header className="App-header">
         <p className="App-logo" alt="logo">Hola Mundo</p>
       </header>
